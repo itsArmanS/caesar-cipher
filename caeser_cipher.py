57fcac4ced4fe8447c9065085c7e0a26fa09b10e
Arman <justtltm8@gmail.com>
2026-05-21 15:02:21 +0200

    v4: added --preserve_case flag, renamed method to cipher/decipher, refactored with ternaries

--- caeser_cipher.py
+++ caeser_cipher.py
@@ -2,32 +2,37 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Cipher or decipher messages using the Caesar Cipher")
 parser.add_argument("message", type=str, help="Input message to cipher/decipher" )
-parser.add_argument("method", type=str, choices=['c', 'd'], default="c", help="Input c to cipher message / d to decipher" )
+parser.add_argument("method", type=str, choices=['cipher', 'decipher'], default="cipher", help="Input cipher to cipher message / decipher to decipher" )
 parser.add_argument("shift", type=int, help="Cipher shift amount (0 - 25)")
+parser.add_argument("--preserve_case", action="store_true", help="Preserve original case (mixed-case output) instead of forcing uppercase")
 
 args = parser.parse_args()
+
+verb = "ciphered" if args.method == "cipher" else "deciphered"
 
 if not 0 <= args.shift <= 25:
     parser.error("Shift must be between 0 and 25")
 
-message = args.message.lower()
-
 ciphered_message = ""
 
-for letter in message:
+for letter in args.message:
     if letter == " ":
         ciphered_message += letter
-    elif letter.isalpha(): 
-        position = ord(letter) - ord("a")
-        if args.method == "c":
+    elif letter.isalpha():     
+
+        anchor = ord("a") if letter.islower() else ord("A")
+        position = ord(letter) - anchor
+            
+        if args.method == "cipher":
             shifted_position = (position + args.shift) % 26
         else:
             shifted_position = (position - args.shift) % 26
 
-        shifted_letter = chr(shifted_position + ord("a"))
+        shifted_letter = chr(shifted_position + anchor)
+
         ciphered_message += shifted_letter
 
-if args.method == "c":
-    print(f"Your ciphered message is: {ciphered_message.upper()}")
-else:
-    print(f"Your deciphered message is: {ciphered_message.upper()}")
+output = ciphered_message if args.preserve_case else ciphered_message.upper()
+print(f"Your {verb} message is: {output}")
+
+    
